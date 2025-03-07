fe16caac69af8efa7fdca350e3ee46faba355eda
Udayraj <110400788+UdayrajJhala@users.noreply.github.com>
2025-03-07 15:06:08 +0530

    changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -118,6 +118,7 @@ const Hero = () => {
       >
         <img
           onWheel={handleWheel}
+          loading="lazy"
           src="https://user-images.githubusercontent.com/74038190/229223263-cf2e4b07-2615-4f87-9c38-e37600f8381a.gif"
           className="w-full max-w-xl mx-auto sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl"
           alt="Art"
